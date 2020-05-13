254c5d16299158b4886a8995fce365933deb5a7b
Diogo Neiss <40122099+diogoneiss@users.noreply.github.com>
2020-05-13 08:57:35 -0300

    adicao de msgs

--- bot.js
+++ bot.js
@@ -11,16 +11,34 @@ function bot() {
     if (questionNum == 0) {
     output.innerHTML = '<h1>Olá, ' + input + '!</h1>';// output response
     document.getElementById("input").value = "";   		// clear text box
-    question = '<h1>how old are you?</h1>';			    	// load next question		
+    question = '<h1>Quantos anos você tem?</h1>';			    	// load next question		
     setTimeout(timedQuestion, 2000);									// output next question after 2sec delay
     }
 
     else if (questionNum == 1) {
-    output.innerHTML = '<h1>That means you were born in ' + (2016 - input) + '</h1>';
+    output.innerHTML = '<h1>Isso significa que você nasceu em ' + (2020 - input) + ' ou '+ (2019 - input) +'</h1>';
     document.getElementById("input").value = "";   
-    question = '<h1>where are you from?</h1>';					      	
+
+    question = '<h1>O que você deseja saber mais sobre? Temos as seguintes opções: </h1>';					      	
     setTimeout(timedQuestion, 2000);
+    question = '<h2> Esconde-Esconde Usando Bots</h2>';					      	
+    setTimeout(timedQuestion, 4000);
+    question = '<h2> Esconde-Esconde Usando Bots</h2> <br> <h2> Replicação da Mente Humana</h2>';					      	
+    setTimeout(timedQuestion, 4000);
+    question = '<h2> Esconde-Esconde Usando Bots</h2> <br> <h2> Replicação da Mente Humana</h2> <br> <h2> O que é Imitation Learning?</h2>';					      	
+    setTimeout(timedQuestion, 4000);
+
+    question = 'Digite o numero da opção que você deseja, entre 1,2 e 3';					      	
+    setTimeout(timedQuestion, 4000);
+
+    question = 'Ainda não implementei daqui em diante';					      	
+    setTimeout(timedQuestion, 4000);
+
+    
     }   
+    else if (questionNum == 2){
+      
+    }
 }
 
 function timedQuestion() {
